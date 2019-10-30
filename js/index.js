32805b1554bba7b176fd9c413c0650f720f31dbe
Vitalik Moroz <vitalik.moroz2@gmail.com>
2019-10-30 11:53:42 +0200

    Lab 7

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// let arr = [1, 2, 3, 1, 4, 6, 7];
+// let result = arr.reduce((acc, val, indx, arr) => {
+//     acc += val;
+//     if (indx == arr.length - 1) acc /= indx;
+//     return acc;
+// }, 0);
+// console.log(result);
+
+console.log("hello1");
+
+setTimeout(() => console.log("hello2"), 0);
+
+console.log("hello3");
